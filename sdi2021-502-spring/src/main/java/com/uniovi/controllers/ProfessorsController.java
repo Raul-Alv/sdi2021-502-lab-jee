d53ddf8372beb0ecf7e1a89b881e5271c82e95a1
Raul <UO265210@uniovi.es>
2021-02-16 11:04:53 +0100

    SDI- IDGIT-session2-Ejercicio-complementario3

--- sdi2021-502-spring/src/main/java/com/uniovi/controllers/ProfessorsController.java
+++ sdi2021-502-spring/src/main/java/com/uniovi/controllers/ProfessorsController.java
@@ -1,6 +1,7 @@
 package com.uniovi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,33 +11,38 @@ import org.springframework.web.bind.annotation.RestController;
 import com.uniovi.entities.Professor;
 import com.uniovi.services.ProfessorService;
 
-@RestController
+@Controller
 public class ProfessorsController {
 
 	@Autowired
 	private ProfessorService professorService;
 	
 	@RequestMapping("/professor/list")
-	public String getList() {
-		return professorService.getProfessors().toString();
+	public String getList(Model model) {
+		model.addAttribute("professorList", professorService.getProfessors());
+		return "professor/list";
 	}
 	
-	@RequestMapping(value="/professor/add")
-	public String setProfessor(@ModelAttribute String dni) {
-		//professorService.addProfessor(professor);
-		professorService.addProfessor(new Professor("32165488M", "Carla", "Perez Iglesias", "Informatica"));
-		return "Ok";
+	@RequestMapping(value="/professor/add", method=RequestMethod.POST)
+	public String setProfessor(@ModelAttribute Professor professor) {
+		professorService.addProfessor(professor);
+		//professorService.addProfessor(new Professor("32165488M", "Carla", "Perez Iglesias", "Informatica"));
+		return "redirect:/professor/list";
 	}
 	
 	@RequestMapping("/professor/details/{dni}" )
-	public String getDetail(@ModelAttribute String dni) {
-		return professorService.getProfessor(dni).toString();
-
+	public String getDetail(Model model, @ModelAttribute String dni) {
+		model.addAttribute("professor", professorService.getProfessor(dni));
+		return "professor/details";
 	}
 	
 	@RequestMapping("/professor/delete/{dni}" )
 	public String deletePorfessor(@ModelAttribute String dni) {
 		professorService.deleteProfessor(dni);
-		return "Ok";
+		return "redirect:/mark/list";
+	}
+	@RequestMapping(value="/professor/add")
+	public String getProfessor(){
+		return "professor/add";
 	}
 }
